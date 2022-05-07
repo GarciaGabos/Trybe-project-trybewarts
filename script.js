3a5dc104246d20cd370917420ff2ff103a6689ae
Ravanz <rayteon.h@gmail.com>
2022-05-07 20:33:59 -0300

    corrigindo js do requisito 3 e 17. Concluindo requisitos até o 19

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const getLoginButton = document.getElementById('login');
-const getInputEmail = document.querySelector('#email');
-const getInputPassword = document.querySelector('#password');
+const getEmail = document.querySelector('#email');
+const getPassword = document.querySelector('#senha');
 
 function loginTrybe() {
-  if (getInputEmail.value === 'login@trybe.com' && getInputPassword.value === 'senha') {
+  if (getEmail.value === 'tryber@teste.com' && getPassword.value === '123456') {
     alert('Olá, Tryber!');
   } else {
-    alert('Email ou senha inválidos');
+    alert('Email ou senha inválidos.');
   }
 } getLoginButton.addEventListener('click', loginTrybe);
+
+const checkbox = document.getElementsByName('toggle');
+const numElementos = checkbox.length;
+const button = document.getElementById('submit-btn');
+for (let i = 0; i < numElementos; i += 1) {
+  checkbox[i].onclick = () => {
+    // essa função passa pelo checkbox, e adiciona o evento ao clicar nele
+    const check = document.querySelector('input[name="toggle"]:checked');
+    // se estiver marcado, vai receber o valor "1" (true)
+    // se não estiver, recebe o valor "0" (false) e desabilita o botão de enviar.
+    button.disabled = check ? 0 : 1;
+  };
+}
