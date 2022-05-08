5fa783a260c027f9e0f249126b9fc496550a23b0
Ravanz <rayteon.h@gmail.com>
2022-05-08 00:04:38 -0300

    simplificando função do requisito 17

--- script.js
+++ script.js
@@ -10,15 +10,13 @@ function loginTrybe() {
   }
 } getLoginButton.addEventListener('click', loginTrybe);
 
-const checkbox = document.getElementsByName('toggle');
-const numElementos = checkbox.length;
-const button = document.getElementById('submit-btn');
-for (let i = 0; i < numElementos; i += 1) {
-  checkbox[i].onclick = () => {
-    // essa função passa pelo checkbox, e adiciona o evento ao clicar nele
-    const check = document.querySelector('input[name="toggle"]:checked');
-    // se estiver marcado, vai receber o valor "1" (true)
-    // se não estiver, recebe o valor "0" (false) e desabilita o botão de enviar.
-    button.disabled = check ? 0 : 1;
-  };
-}
+const checked = document.getElementById('agreement');
+// função para conferir se o checkbox está marcado e ativar botão submit
+function checkbox() {
+  if (document.getElementById('agreement').checked === true) {
+    document.getElementById('submit-btn').disabled = '';
+  }
+  if (document.getElementById('agreement').checked === false) {
+    document.getElementById('submit-btn').disabled = 'disabled';
+  } return checkbox;
+} checked.addEventListener('click', checkbox);
